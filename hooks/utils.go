ec38456df910ba4d8cdf8b95f15c94149b973c7a
Siggi Skulason <siggi.skulason@canonical.com>
2021-04-27 18:40:14 +0100

    feat(snap): add go-based hooks for Kong user and TLS certificate
    This feature provides the ability to
    - set TLS configuration of Kong and
    - add a single Kong user
    
    1) The kong user can be set using the username, ID and algorithm:
    
    sudo snap set edgexfoundry env.security-proxy.user=user01,USER_ID,ES256
    sudo snap set edgexfoundry env.security-proxy.public-key="$(cat public.pem)"
    
    2) A different user can then be set by first removing the first one
    
    sudo snap set edgexfoundry env.security-proxy.user=""
    sudo snap set edgexfoundry env.security-proxy.public-key=""
    sudo snap set edgexfoundry env.security-proxy.user=user02,USER_ID2,ES256
    sudo snap set edgexfoundry env.security-proxy.public-key="$(cat public.pem)"
    
    3) The TLS certificate and private key can be set with
    
    sudo snap set edgexfoundry env.security-proxy.tls-certificate="$(cat localhost.cert)"
    sudo snap set edgexfoundry env.security-proxy.tls-private-key="$(cat localhost.key)"
    
    4) To set a new TLS certificate first clear the values and then set them again.
    If the certificate is for a different SNI than localhost, then set the optional
    sni value first:
    
    sudo snap set edgexfoundry env.security-proxy.tls-certificate=""
    sudo snap set edgexfoundry env.security-proxy.tls-private-key=""
    
    sudo snap set edgexfoundry env.security-proxy.tls-sni=myhostname
    sudo snap set edgexfoundry env.security-proxy.tls-certificate="$(cat myhostname.cert)"
    sudo snap set edgexfoundry env.security-proxy.tls-private-key="$(cat myhostname.key)"
    
    Signed-off-by: Siggi Skulason <siggi.skulason@canonical.com>

--- hooks/utils.go
+++ hooks/utils.go
@@ -273,6 +273,150 @@ func flattenConfigJSON(p string, k string, vJSON interface{}, flatConf map[strin
 	}
 }
 
+const userSemaphoreFile = ".secrets-config-user"
+const tlsSemaphoreFile = ".secrets-config-tls"
+
+// Write a security-proxy file
+func securityProxyWriteFile(filename, contents string) (path string, err error) {
+	path = fmt.Sprintf("%s/secrets/edgex-security-proxy-setup/%s", SnapData, filename)
+	err = ioutil.WriteFile(path, []byte(contents), 0600)
+	if err == nil {
+		Debug(fmt.Sprintf("Wrote file '%s'", path))
+	} else {
+		err = fmt.Errorf("failed to write file %s - %v", path, err)
+	}
+	return
+}
+
+// Read a security-proxy file
+func securityProxyReadFile(filename string) (contents string, err error) {
+	path := fmt.Sprintf("%s/secrets/edgex-security-proxy-setup/%s", SnapData, filename)
+	bytes, err := ioutil.ReadFile(path)
+	if err == nil {
+		contents = string(bytes)
+		Debug(fmt.Sprintf("Read file '%s'", path))
+	} else {
+		err = fmt.Errorf("failed to read file %s - %v", path, err)
+	}
+	return
+}
+
+// Delete a security-proxy semaphore file
+func securityProxyRemoveSemaphore(filename string) (err error) {
+	path := fmt.Sprintf("%s/secrets/edgex-security-proxy-setup/%s", SnapData, filename)
+	err = os.Remove(path)
+	if err == nil {
+		Debug("Removed file '" + path + "'")
+	} else {
+		Debug(fmt.Sprintf("Could not remove file '%s' : %v", path, err))
+	}
+	return
+}
+
+// Execute the secrets-config tool with the given arguments
+func securityProxyExecSecretsConfig(args []string) error {
+	service := "security-proxy-setup"
+	cmdSecretsConfig := exec.Command("secrets-config", args...)
+	cmdSecretsConfig.Dir = fmt.Sprintf("%s/%s", SnapDataConf, service)
+	out, err := cmdSecretsConfig.Output()
+	Debug("Executed \"secrets-config " + fmt.Sprint(args) + "\" result=" + string(out))
+	return err
+}
+
+// Remove the semaphore, so that we can set the certificate again
+func securityProxyDeleteCurrentTLSCertIfSet() error {
+
+	return securityProxyRemoveSemaphore(tlsSemaphoreFile)
+}
+
+// Delete the current user - if one has been set up
+func securityProxyDeleteCurrentUserIfSet() error {
+	service := "security-proxy-setup"
+	// if no user has been set up, then ignore the request
+	username, err := securityProxyReadFile(userSemaphoreFile)
+	if err != nil {
+		Debug("proxy: No user has been set up")
+		return nil
+	}
+
+	args := []string{"proxy", "deluser", "--user", username}
+	cmdSecretsConfig := exec.Command("secrets-config", args...)
+	cmdSecretsConfig.Dir = fmt.Sprintf("%s/%s", SnapDataConf, service)
+	out, err := cmdSecretsConfig.Output()
+	if err != nil {
+		return err
+	}
+
+	securityProxyRemoveSemaphore(userSemaphoreFile)
+	Debug("Executed \"secrets-config " + fmt.Sprint(args) + "\" result=" + string(out))
+	Info("proxy: Removed current user")
+	return nil
+}
+
+// Set up the proxy with the specified user.
+func securityProxyAddUser(jwtUsername, jwtUserID, jwtAlgorithm, jwtPublicKey string) error {
+	currentUser, err := securityProxyReadFile(userSemaphoreFile)
+	if err == nil && currentUser != "" {
+		if currentUser == jwtUsername {
+			//	If a user has already been set - and it's the same user - then silently ignore the request
+			Debug("proxy: Ignoring request to set up same user again")
+			return nil
+		} else {
+			// if this is a different user, then return an error
+			return fmt.Errorf("the proxy user has already been set. To add a new user, first delete the current user by setting 'user' and 'public-key' to an empy string")
+		}
+	}
+
+	publicKeyFilePath, err := securityProxyWriteFile("jwt-user-public-key.pem", jwtPublicKey)
+	if err != nil {
+		return err
+	}
+	args := []string{"proxy", "adduser", "--token-type", "jwt", "--user", jwtUsername, "--id", jwtUserID, "--algorithm", jwtAlgorithm, "--public_key", publicKeyFilePath}
+	err = securityProxyExecSecretsConfig(args)
+	if err != nil {
+		return fmt.Errorf("failed to create user - %v", err)
+	}
+	_, err = securityProxyWriteFile(userSemaphoreFile, jwtUsername)
+	if err != nil {
+		return err
+	}
+	Info("proxy: Added new user")
+	return nil
+}
+
+// Set the TLS certificate. If a certificate has already been set then silently ignore the request
+func securityProxySetTLSCertificate(tlsCertificate, tlsPrivateKey, tlsSNI string) error {
+	_, err := securityProxyReadFile(tlsSemaphoreFile)
+	if err == nil {
+		Debug("The TLS certificate has already been set. To set it again, first set tls-certificate and tls-private-key to an empty string")
+		return nil
+	}
+	tlsCertFilename, err := securityProxyWriteFile("tls-certificate.pem", tlsCertificate)
+	if err != nil {
+		return err
+	}
+	tlsPrivateKeyFilename, err := securityProxyWriteFile("tls-private-key.pem", tlsPrivateKey)
+	if err != nil {
+		return err
+	}
+	if tlsSNI != "" {
+		args := []string{"proxy", "tls", "--incert", tlsCertFilename, "--inkey", tlsPrivateKeyFilename, "--snis", tlsSNI}
+		err = securityProxyExecSecretsConfig(args)
+	} else {
+		args := []string{"proxy", "tls", "--incert", tlsCertFilename, "--inkey", tlsPrivateKeyFilename}
+		err = securityProxyExecSecretsConfig(args)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to set TLS certificate - %v", err)
+	}
+	_, err = securityProxyWriteFile(tlsSemaphoreFile, "TLS certificate set")
+	if err != nil {
+		return err
+	}
+	Info("New TLS Certificate and private key set")
+	return nil
+}
+
 // HandleEdgeXConfig processes snap configuration which can be used to override
 // edgexfoundry configuration via environment variables sourced by the snap
 // service wrapper script.
@@ -295,7 +439,46 @@ func HandleEdgeXConfig(service, envJSON string) error {
 	}
 
 	b := bytes.Buffer{}
+
+	var jwtUsername, jwtUserID, jwtAlgorithm, jwtPublicKey string
+	var tlsCertificate, tlsPrivateKey, tlsSNI string
+
 	for k, v := range flatConf {
+
+		// a couple of special cases for security-proxy, to create an user/token and set the TLS cert.
+		// This uses the standard naming schema but doesn't actually use environment variables
+		if service == "security-proxy" {
+			value := strings.TrimSpace(v)
+			switch k {
+			case "user":
+				if value != "" {
+					s := strings.Split(value, ",")
+					if len(s) != 3 {
+						return fmt.Errorf("security-proxy.user expects a value containing 'username,userID,algorithm'. Example: 'me,1234,ES256' but got " + fmt.Sprint(s))
+					}
+					jwtUsername = strings.TrimSpace(s[0])
+					jwtUserID = strings.TrimSpace(s[1])
+					jwtAlgorithm = strings.ToUpper(strings.TrimSpace(s[2]))
+					if jwtAlgorithm != "ES256" && jwtAlgorithm != "RS256" {
+						return fmt.Errorf("invalid algorithm value - should be ES256 or RS256")
+					}
+				}
+				continue
+			case "public-key":
+				jwtPublicKey = value
+				continue
+			case "tls-certificate":
+				tlsCertificate = value
+				continue
+			case "tls-private-key":
+				tlsPrivateKey = value
+				continue
+			case "tls-sni":
+				tlsSNI = value
+				continue
+			}
+		}
+
 		env, ok := ConfToEnv[k]
 		if !ok {
 			return errors.New("invalid EdgeX config option - " + k)
@@ -316,6 +499,29 @@ func HandleEdgeXConfig(service, envJSON string) error {
 	// to translate the hook name to the actual binary name.
 	if service == "security-proxy" {
 		service = "security-proxy-setup"
+
+		if jwtUsername == "" && jwtPublicKey == "" {
+			// if the values have been set to "" then delete the current user
+			securityProxyDeleteCurrentUserIfSet()
+		} else if jwtUsername != "" && jwtPublicKey != "" {
+			// else add a new user
+			err = securityProxyAddUser(jwtUsername, jwtUserID, jwtAlgorithm, jwtPublicKey)
+			if err != nil {
+				return err
+			}
+		}
+
+		if tlsCertificate == "" && tlsPrivateKey == "" {
+			// if the values have been set to "" then clear the semaphore so that a new cert can be set
+			securityProxyDeleteCurrentTLSCertIfSet()
+		} else if tlsCertificate != "" && tlsPrivateKey != "" {
+			// Set the TLS certificate and private key
+			err = securityProxySetTLSCertificate(tlsCertificate, tlsPrivateKey, tlsSNI)
+			if err != nil {
+				return err
+			}
+		}
+
 	} else if service == "security-secret-store" {
 		service = "security-secretstore-setup"
 	}
@@ -325,7 +531,7 @@ func HandleEdgeXConfig(service, envJSON string) error {
 
 	err = ioutil.WriteFile(tmp, b.Bytes(), 0644)
 	if err != nil {
-		return fmt.Errorf("failed to write %.env file - %v", service, err)
+		return fmt.Errorf("failed to write %s.env file - %v", service, err)
 	}
 
 	err = os.Rename(tmp, path)
